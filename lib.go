01beaeb42d651f2f84173dca8f0d6078dc7fc1be
Yen-Hsiang Chang <yhchang3@illinois.edu>
2021-01-16 17:45:17 +0800

    update lib

--- lib.go
+++ lib.go
@@ -8,7 +8,7 @@ package pytorch
 // #cgo !python CXXFLAGS: -isystem /opt/libtorch/include/torch/csrc
 // #cgo !python LDFLAGS: -lgomp -L/opt/libtorch/lib
 // #cgo linux,amd64,!nogpu CFLAGS: -I/usr/local/cuda/include
-// #cgo linux,amd64,!nogpu LDFLAGS: -L/usr/local/cuda/lib64 -lcuda -lcudart -lcublas -lcudnn -lcaffe2_nvrtc -lnvrtc-builtins -lnvrtc -lnvToolsExt -L/opt/libtorch/lib -lc10_cuda
+// #cgo linux,amd64,!nogpu LDFLAGS: -L/usr/local/cuda/lib64 -lcuda -lcudart -lcublas -lcudnn -lcaffe2_nvrtc -lnvrtc-builtins -lnvrtc -lnvToolsExt -L/opt/libtorch/lib -Wl,--no-as-needed -lc10_cuda -ltorch_cuda
 // #cgo python CXXFLAGS: -isystem /usr/local/anaconda3/lib/python3.6/site-packages/torch/include/torch/csrc/api/include
 // #cgo python CXXFLAGS: -isystem /usr/local/anaconda3/lib/python3.6/site-packages/torch/include
 // #cgo python CXXFLAGS: -isystem /usr/local/anaconda3/lib/python3.6/site-packages/torch/include/torch/csrc
